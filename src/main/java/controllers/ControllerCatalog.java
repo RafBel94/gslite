2c85bae6a3c08b54c4ce672ca1b6650da73a780c
RafBel94 <rafael.beltrancaceres@gmail.com>
2024-10-02 20:48:03 +0200

    GUICatalog and ControllerCatalog

---
+++ src/main/java/controllers/ControllerCatalog.java
@@ -0,0 +1,68 @@
+package controllers;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.List;
+
+import javax.swing.JButton;
+import javax.swing.JList;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
+
+import views.GUICatalog;
+
+public class ControllerCatalog {
+	private GUICatalog guiCatalog;
+	private List<Product> productList;
+	private int selectedIndex = 0;
+
+	public ControllerCatalog(GUICatalog guiCatalog) {
+		super();
+		this.guiCatalog = guiCatalog;
+		
+		guiCatalog.addActListener(new ActListener());
+		guiCatalog.addItemSelectListener(new ListSelectListener());
+		guiCatalog.addDocuListener(new DocuListener());
+	}
+	
+	private class ActListener implements ActionListener {
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			JButton btn = (JButton) e.getSource();
+			
+			//TODO: new GUIProduct(guiCatalog)
+		}
+		
+	}
+	
+	private class ListSelectListener implements ListSelectionListener {
+
+		@Override
+		public void valueChanged(ListSelectionEvent e) {
+			JList list = (JList) e.getSource();
+			selectedIndex = list.getSelectedIndex();
+			
+			//TODO: Load Product info into lbl_productName and area_description
+		}
+		
+	}
+	
+	private class DocuListener implements DocumentListener {
+
+		@Override
+		public void insertUpdate(DocumentEvent e) {
+		}
+
+		@Override
+		public void removeUpdate(DocumentEvent e) {
+		}
+
+		@Override
+		public void changedUpdate(DocumentEvent e) {
+		}
+		
+	}
+}
---
+++ src/main/java/views/GUICatalog.java
@@ -0,0 +1,221 @@
+package views;
+
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JList;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JSeparator;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+import javax.swing.SwingConstants;
+import javax.swing.border.EmptyBorder;
+import javax.swing.border.LineBorder;
+import javax.swing.event.DocumentListener;
+import javax.swing.event.ListSelectionListener;
+
+import controllers.ControllerCatalog;
+
+public class GUICatalog extends JFrame {
+
+	private static final long serialVersionUID = 1L;
+	private JPanel contentPane;
+	private JPanel panel_searchBar;
+	private JLabel lbl_searchBar;
+	private JTextField txt_searchBar;
+	private JSeparator separator_searchbar;
+	private JScrollPane scroll_catalog;
+	private JList list_catalog;
+	private JLabel lbl_image;
+	private JSeparator separator_list;
+	private JLabel lbl_product;
+	private JLabel lbl_productName;
+	private JLabel lbl_description;
+	private JTextArea area_description;
+	private JButton btn_details;
+
+	public GUICatalog() {
+		setTitle("Catalog");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 675, 570);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+
+		panel_searchBar = new JPanel();
+		panel_searchBar.setBounds(10, 11, 639, 51);
+		contentPane.add(panel_searchBar);
+		panel_searchBar.setLayout(null);
+
+		lbl_searchBar = new JLabel("Search:");
+		lbl_searchBar.setFont(new Font("Tahoma", Font.PLAIN, 13));
+		lbl_searchBar.setBounds(9, 7, 107, 14);
+		panel_searchBar.add(lbl_searchBar);
+
+		txt_searchBar = new JTextField();
+		txt_searchBar.setFont(new Font("Tahoma", Font.PLAIN, 13));
+		txt_searchBar.setBounds(9, 25, 619, 20);
+		panel_searchBar.add(txt_searchBar);
+		txt_searchBar.setColumns(10);
+
+		separator_searchbar = new JSeparator();
+		separator_searchbar.setBounds(20, 73, 617, 10);
+		contentPane.add(separator_searchbar);
+
+		scroll_catalog = new JScrollPane();
+		scroll_catalog.setBorder(new LineBorder(Color.LIGHT_GRAY));
+		scroll_catalog.setBounds(20, 94, 268, 426);
+		contentPane.add(scroll_catalog);
+
+		list_catalog = new JList();
+		scroll_catalog.setViewportView(list_catalog);
+
+		lbl_image = new JLabel("");
+		lbl_image.setBorder(new LineBorder(Color.LIGHT_GRAY));
+		lbl_image.setBounds(372, 94, 230, 230);
+		contentPane.add(lbl_image);
+
+		separator_list = new JSeparator();
+		separator_list.setOrientation(SwingConstants.VERTICAL);
+		separator_list.setBounds(308, 94, 13, 426);
+		contentPane.add(separator_list);
+
+		lbl_product = new JLabel("Product:");
+		lbl_product.setFont(new Font("Tahoma", Font.PLAIN, 13));
+		lbl_product.setBounds(323, 356, 107, 19);
+		contentPane.add(lbl_product);
+
+		lbl_productName = new JLabel("No product selected");
+		lbl_productName.setFont(new Font("Tahoma", Font.BOLD, 13));
+		lbl_productName.setBounds(323, 373, 311, 23);
+		contentPane.add(lbl_productName);
+
+		lbl_description = new JLabel("Description:");
+		lbl_description.setFont(new Font("Tahoma", Font.PLAIN, 13));
+		lbl_description.setBounds(323, 399, 116, 19);
+		contentPane.add(lbl_description);
+
+		area_description = new JTextArea();
+		area_description.setBorder(new LineBorder(Color.LIGHT_GRAY));
+		area_description.setBounds(326, 423, 311, 97);
+		contentPane.add(area_description);
+
+		btn_details = new JButton("View details");
+		btn_details.setFont(new Font("Tahoma", Font.PLAIN, 13));
+		btn_details.setBounds(429, 329, 116, 25);
+		contentPane.add(btn_details);
+
+		new ControllerCatalog(this);
+	}
+
+	public void addActListener(ActionListener actListener) {
+		btn_details.addActionListener(actListener);
+	}
+
+	public void addDocuListener(DocumentListener docuListener) {
+		txt_searchBar.getDocument().addDocumentListener(docuListener);
+	}
+
+	public void addItemSelectListener(ListSelectionListener selectListener) {
+		list_catalog.addListSelectionListener(selectListener);
+	}
+
+	public JPanel getPanel_searchBar() {
+		return panel_searchBar;
+	}
+
+	public void setPanel_searchBar(JPanel panel_searchBar) {
+		this.panel_searchBar = panel_searchBar;
+	}
+
+	public JLabel getLbl_searchBar() {
+		return lbl_searchBar;
+	}
+
+	public void setLbl_searchBar(JLabel lbl_searchBar) {
+		this.lbl_searchBar = lbl_searchBar;
+	}
+
+	public JTextField getTxt_searchBar() {
+		return txt_searchBar;
+	}
+
+	public void setTxt_searchBar(JTextField txt_searchBar) {
+		this.txt_searchBar = txt_searchBar;
+	}
+
+	public JScrollPane getScroll_catalog() {
+		return scroll_catalog;
+	}
+
+	public void setScroll_catalog(JScrollPane scroll_catalog) {
+		this.scroll_catalog = scroll_catalog;
+	}
+
+	public JList getList_catalog() {
+		return list_catalog;
+	}
+
+	public void setList_catalog(JList list_catalog) {
+		this.list_catalog = list_catalog;
+	}
+
+	public JLabel getLbl_image() {
+		return lbl_image;
+	}
+
+	public void setLbl_image(JLabel lbl_image) {
+		this.lbl_image = lbl_image;
+	}
+
+	public JLabel getLbl_product() {
+		return lbl_product;
+	}
+
+	public void setLbl_product(JLabel lbl_product) {
+		this.lbl_product = lbl_product;
+	}
+
+	public JLabel getLbl_productName() {
+		return lbl_productName;
+	}
+
+	public void setLbl_productName(JLabel lbl_productName) {
+		this.lbl_productName = lbl_productName;
+	}
+
+	public JLabel getLbl_description() {
+		return lbl_description;
+	}
+
+	public void setLbl_description(JLabel lbl_description) {
+		this.lbl_description = lbl_description;
+	}
+
+	public JTextArea getArea_description() {
+		return area_description;
+	}
+
+	public void setArea_description(JTextArea area_description) {
+		this.area_description = area_description;
+	}
+
+	public JButton getBtn_details() {
+		return btn_details;
+	}
+
+	public void setBtn_details(JButton btn_details) {
+		this.btn_details = btn_details;
+	}
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+}
